ad3242d129d6b377bfdeff0df3bc78c6ae48aab6
MariahAdams <mariah.adams23@yahoo.com>
2018-09-12 18:22:17 -0700

    file organization

--- src/components/header.js
+++ src/components/header.js
@@ -1,3 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default props => <h1>{props.headerText}</h1>
+class Header extends Component {
+  
+  render() { 
+    return (
+      <div>
+        <nav>
+          <span className="menu-toggle">Menu <i className="fas fa-bars"></i></span>
+          <div className="menu-content">
+            <a href="#projects">Projects</a>
+            <a href="#about">About</a>
+            <a href="#resume">Resume</a>
+          </div>
+        </nav>
+    </div>
+    );
+  }
+}
+ 
+export default Header;
