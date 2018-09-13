82feb4d8922477f2dce6b133300e0232ef38c9c6
MariahAdams <mariah.adams23@yahoo.com>
2018-09-13 07:06:01 -0700

    wiring up about content, adding projects page

--- src/pages/about.js
+++ src/pages/about.js
@@ -18,6 +18,31 @@ class About extends Component {
       <div>
         <h1>{name}</h1>
         <p>{body.body}</p>
+
+        <section className="about">
+        <hgroup>
+          <h2>About Mariah.</h2>
+          <img src="https://avatars3.githubusercontent.com/u/35393465?s=460&v=4" alt="Mariah Adams" />
+        </hgroup>
+        
+        <article>
+          <p>
+            Mariah is a Full Stack JavaScript Developer currently living in the Pacific Northwest. She has a degree in business management from San Diego State University and experience in hospitality. 
+          </p>
+          <p>
+            Ever since she completed the Disney College Program, it’s been her dream to create magical moments though code. She brings strong organizational skills, teamwork, and a desire to continuously learn to her work. 
+          </p>
+          <p>
+            She is currently finishing up an intensive program with Alchemy Code Lab in Portland, Oregon. When she is not coding, you can find her baking, out in search of a good cup of coffee, or planning her next trip to Disney.
+          </p>
+        </article>
+  
+        <div className="social-media">
+          <a href="https://github.com/MariahAdams">GI</a>
+          <a href="https://www.linkedin.com/in/mariah-adams/">LI</a>
+          <a href="#Instagram">Insta</a>
+        </div>
+      </section>
       </div>
     );
   }
