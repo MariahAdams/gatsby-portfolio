faa16b8909c004c41a57c04c752f1945b5a61d57
MariahAdams <mariah.adams23@yahoo.com>
2018-09-13 15:18:52 -0700

    renaming files

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,28 @@
-import React from 'react'
-import { graphql, StaticQuery } from 'gatsby'
+import React from 'react';
+import { graphql, StaticQuery } from 'gatsby';
+
 
 export default () => (
   <StaticQuery
     query={graphql`
-      query {
+      query bio {
         allContentfulBio {
           edges {
             node {
-              name
+              name,
+              picture {
+                file {
+                  url,
+                  details {
+                    size
+                  },
+                  fileName,
+                  contentType
+                }
+              },
+              body {
+                body
+              }
             }
           }
         }
@@ -16,9 +30,22 @@ export default () => (
     `
 }
     render={data => (
-      <div>
-        {data.allContentfulBio.edges[0].node.name}
+      <section>
+        <hgroup>
+        <h2>About Mariah.</h2>
+        <img src={data.allContentfulBio.edges[0].node.picture.file.url} alt="Mariah Adams"></img>
+      </hgroup>
+      
+      <article>
+      {data.allContentfulBio.edges[0].node.body.body}
+      </article>
+
+      <div className="social-media">
+        <a href="https://github.com/MariahAdams">github</a>
+        <a href="https://www.linkedin.com/in/mariah-adams/">linkedin</a>
+        <a href="#Instagram">Instagram</a>
       </div>
+      </section>
     )}
   />
 )
