60883a4c9476a6a5ee07ce63921d2cedab0553ea
MariahAdams <mariah.adams23@yahoo.com>
2018-09-15 15:16:11 -0700

    more styling

--- src/components/projects/ProjectsList.js
+++ src/components/projects/ProjectsList.js
@@ -1,21 +1,38 @@
 import React, { Component } from 'react';
+import styles from './Projects.scss';
 
 class ProjectsList extends Component {
   
   render() { 
     return (
-      <section className="projects">
+      <section className={styles.projects}>
         <header>
           <h2>My Projects.</h2>
         </header>
-  
-        <article>
-          <a href="./project-detail.html"><img src="" alt="splash page for project 1" /></a>
-        </article>
-  
-        <article>
-          <a href="./project-detail.html"><img src="" alt="splash page for project 2" /></a>
-        </article>
+
+        <div>
+          <article>
+            <a href="./project-detail.html"><img src="" alt="splash page for project 1" /></a>
+          </article>
+    
+          <article>
+            <a href="./project-detail.html"><img src="" alt="splash page for project 2" /></a>
+          </article>
+          <article>
+            <a href="./project-detail.html"><img src="" alt="splash page for project 1" /></a>
+          </article>
+    
+          <article>
+            <a href="./project-detail.html"><img src="" alt="splash page for project 2" /></a>
+          </article>
+          <article>
+            <a href="./project-detail.html"><img src="" alt="splash page for project 1" /></a>
+          </article>
+    
+          <article>
+            <a href="./project-detail.html"><img src="" alt="splash page for project 2" /></a>
+          </article>
+        </div>
       </section>
     );
   }
